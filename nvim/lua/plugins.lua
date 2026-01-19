9d1e06c7defd3ebd51d3740726de64ef4dfa93f7
tom <122504901+wehr-to@users.noreply.github.com>
2026-01-18 21:14:36 -0600

    Create plugins.lua

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.9",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = "Neotree",
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Neo-tree explorer" },
+    },
+    config = function()
+      require("neo-tree").setup({})
+    end,
+  },
+}
