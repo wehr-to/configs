3ff258ca593fb793811fcd5c81a4fc29c4cf690b
tom <122504901+wehr-to@users.noreply.github.com>
2026-01-19 21:26:29 -0600

    Enhance telescope configuration with ui-select
    Updated telescope plugin configuration to include ui-select extension and key mappings.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,22 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.9",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  keys = {
-    { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.9",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = require("telescope.themes").get_dropdown({}),
+        },
+      })
+      require("telescope").load_extension("ui-select")
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    end,
   },
 }
